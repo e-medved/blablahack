98dff0b60b0c4c702c61b811e27cb429c48f7f93
Medvedev Evgeney <petia256@yandex.ru>
2016-10-11 23:26:46 +0300

    REST service sketch

---
+++ src/main/java/org/project/blablahack/BlaUser.java
@@ -0,0 +1,20 @@
+package org.project.blablahack;
+
+
+public class BlaUser {
+  private String balHash;
+  private String phone;
+
+  public BlaUser(String balHash, String phone) {
+    this.balHash = balHash;
+    this.phone = phone;
+  }
+
+  public String getBalHash() {
+    return balHash;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+}
---
+++ src/main/java/org/project/blablahack/BlaUserController.java
@@ -0,0 +1,20 @@
+package org.project.blablahack;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BlaUserController {
+
+  @RequestMapping
+  public BlaUser getUserByHash(@RequestParam(value = "hash", defaultValue = "") String blaHash){
+    return new BlaUser("0xooooQ", "TODO");
+  }
+
+  @RequestMapping
+  public void saveBlaUsers(){
+
+  }
+}
