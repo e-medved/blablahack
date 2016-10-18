29b6f53ee257e4f46847666298b278cb5301c91a
Medvedev Evgeney <petia256@yandex.ru>
2016-10-19 00:54:26 +0300

    web - rest client operations

--- web/blaextension/background.js
+++ web/blaextension/background.js
@@ -5,13 +5,27 @@ chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
         sendUsers(message);
 });
 
+var baseServiceUrl = "http://127.0.0.1:8080/";
+
 function sendUsers(message){
     $.ajax({
+        url: baseServiceUrl + "users",
         type: 'post',
         data: JSON.stringify(message.passengers),
         contentType: 'application/json',
         dataType: 'json'
     });
+}
+
+function getUserFromServerByHash(hashID){
+
+    $.ajax({
+        url: baseServiceUrl + "users/" + hashID,
+        type: 'get',
+        contentType: 'application/json',
+        dataType: 'json'
+    });
+
 }
 
 function saveTripIdToLocalStorage(){
