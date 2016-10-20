51787884900e530ddad063a4e538561abd4f7755
Medvedev Evgeney <petia256@yandex.ru>
2016-10-20 23:23:41 +0300

    controller - work get request

--- src/main/java/org/project/blablahack/rest/BlaUserController.java
+++ src/main/java/org/project/blablahack/rest/BlaUserController.java
@@ -1,5 +1,6 @@
 package org.project.blablahack.rest;
 
+import org.apache.log4j.Logger;
 import org.project.blablahack.model.BlaUser;
 import org.project.blablahack.service.BlaUserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,23 +14,23 @@ import java.util.List;
 @RestController
 public class BlaUserController {
 
+  private final Logger log = Logger.getLogger(org.project.blablahack.rest.BlaUserController.class.getName());
+
   @Autowired
   BlaUserService userService;
 
-  @RequestMapping("/users/")
-  public ResponseEntity<BlaUser> getUserByHash(@RequestParam(value = "hash", defaultValue = "") String blaHash){
+  //curl -I http://127.0.0.1:8080/users/UoYLcS9LVkUCvi36lnPeQg
+  @GetMapping("/users/{hash}")
+  public ResponseEntity<BlaUser> getUserByHash(@PathVariable("hash") String blaHash){
     BlaUser user = userService.get(blaHash);
-    if (user != null)
+    if (user != null) {
+      log.info("Finded user with ID: " + blaHash);
       return new ResponseEntity<BlaUser>(user, HttpStatus.OK);
-    else
+    }
+    else {
+      log.info("User with ID: " + blaHash + "not found");
       return new ResponseEntity<BlaUser>(HttpStatus.NOT_FOUND);
-  }
-
-  @RequestMapping(value = "/users", method = RequestMethod.POST)
-  public ResponseEntity<BlaUser> saveBlaUsers(@RequestBody BlaUser user){
-    userService.save(user);
-
-    return new ResponseEntity<BlaUser>(user, HttpStatus.OK);
+    }
   }
 
   @RequestMapping(value = "/users", method = RequestMethod.POST)
